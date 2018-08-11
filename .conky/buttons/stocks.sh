dbae8f6634242033d4513ecf4446b33989b6b0a3
mrossinek <rmax@ethz.ch>
2018-08-11 23:53:18 +0200

    [conky] adapt size and bg of stock preview

--- .conky/buttons/stocks.sh
+++ .conky/buttons/stocks.sh
@@ -2,5 +2,6 @@
 (
 echo "Current Stock prices"
 cat ~/.stocks/prices.txt
-) | dzen2 -p -x 1600 -y 20 -w 300 -l 11 -sa l -ta c \
-    -e 'onstart=uncollapse;button1=exit;button3=exit'
+) | dzen2 -p -x 1600 -y 20 -w 295 -l 11 -sa l -ta c \
+        -bg "#1c1c1c" \
+        -e 'onstart=uncollapse;button1=exit;button3=exit'
