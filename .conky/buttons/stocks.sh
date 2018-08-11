305b449d53f11bf6698664260bbd264476b50e88
mrossinek <rmax@ethz.ch>
2018-08-11 20:10:13 +0200

    [conky] organize scripts and buttons

--- .conky/buttons/stocks.sh
+++ .conky/buttons/stocks.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 (
 echo "Current Stock prices"
-cat ~/.conky/stock-prices.txt
+cat ~/.stocks/prices.txt
 ) | dzen2 -p -x 1600 -y 20 -w 300 -l 11 -sa l -ta c \
     -e 'onstart=uncollapse;button1=exit;button3=exit'
