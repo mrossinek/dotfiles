d891cfa47c9a81f3c4ceb41ce15b33fff060725e
mrossinek <rmax@ethz.ch>
2018-08-13 18:20:17 +0200

    [conky] move pop-ups to relative positioning

--- .conky/buttons/stocks.sh
+++ .conky/buttons/stocks.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+x_pos=$(xrandr | grep -o 'current [0-9]*\ x' | sed 's/[a-z ]//g')
+x_pos=$(( $x_pos - 300 ))
 (
 echo "Current Stock prices"
 cat ~/.stocks/prices.txt
-) | dzen2 -p -x 1600 -y 20 -w 295 -l 11 -sa l -ta c \
+) | dzen2 -p -x $x_pos -y 20 -w 295 -l 11 -sa l -ta c \
         -bg "#1c1c1c" \
         -e 'onstart=uncollapse;button1=exit;button3=exit'
