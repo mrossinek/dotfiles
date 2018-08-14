34550c8b4a6cfe81d12a9c4d04e9d9a54ea98c15
mrossinek <rmax@ethz.ch>
2018-08-14 16:50:02 +0200

    [conky] show playlist instead of music controls due to lagginess

--- .conky/buttons/music.sh
+++ .conky/buttons/music.sh
@@ -2,8 +2,10 @@
 x_pos=$(xrandr | grep -o 'current [0-9]*\ x' | sed 's/[a-z ]//g')
 x_pos=$(( $x_pos - 1350 ))
 (
-echo "Music Controls"
-conky -c ~/.conky/buttons/music.conkyrc
-) | dzen2 -p -x $x_pos -y 20 -w 330 -l 3 \
+#echo "Music Controls"
+#conky -c ~/.conky/buttons/music.conkyrc
+echo "MPC Playlist"
+mpc playlist
+) | dzen2 -p -x $x_pos -y 20 -w 330 -l 10 \
         -bg "#1c1c1c" \
         -e 'onstart=uncollapse;button3=exit'
