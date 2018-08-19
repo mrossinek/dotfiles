31ca1e4ecf0b873588d66bf4564f911c0a4eed49
mrossinek <rmax@ethz.ch>
2018-08-19 14:59:50 +0200

    [conky] split system monitoring

--- .conky/buttons/ram.sh
+++ .conky/buttons/ram.sh
@@ -2,8 +2,8 @@
 x_pos=$(xrandr | grep -o 'current [0-9]*\ x' | sed 's/[a-z ]//g')
 x_pos=$(( $x_pos - 800 ))
 (
-echo "Top 10 CPU consumers"
-ps aux --sort -%cpu | head -10
+echo "Top 10 RAM consumers"
+ps aux --sort -rss | head -10
 ) | dzen2 -p -x $x_pos -y 20 -w 600 -l 10 \
         -bg "#1c1c1c" \
         -e 'onstart=uncollapse;button3=exit'
