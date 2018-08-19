4059e7ff1f5a8521c87bcc81c42b01cce02c559a
mrossinek <rmax@ethz.ch>
2018-08-19 22:13:37 +0200

    [conky] fix ram.sh

--- .conky/buttons/ram.sh
+++ .conky/buttons/ram.sh
@@ -3,7 +3,7 @@ x_pos=$(xrandr | grep -o 'current [0-9]*\ x' | sed 's/[a-z ]//g')
 x_pos=$(( $x_pos - 800 ))
 (
 echo "Top 10 RAM consumers"
-ps aux --sort -rss | head -10
+ps aux --sort -%mem | head -10
 ) | dzen2 -p -x $x_pos -y 20 -w 600 -l 10 \
         -bg "#1c1c1c" \
         -e 'onstart=uncollapse;button3=exit'
