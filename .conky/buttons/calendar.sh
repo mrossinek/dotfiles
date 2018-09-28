690b5d03f2f9eac6d8fafb5aaa1a992e944685bb
mrossinek <rmax@ethz.ch>
2018-09-28 15:50:31 +0200

    [conky] limit number of lines in popups

--- .conky/buttons/calendar.sh
+++ .conky/buttons/calendar.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 x_pos=$(xrandr | grep -o 'current [0-9]*\ x' | sed 's/[a-z ]//g')
 x_pos=$(( $x_pos - 300 ))
+cnt=$(khal list 2>/dev/null | wc -l)
+cnt=$(( $cnt<15?$cnt:15 ))
 (
 echo "Upcoming Events"
-khal list 2>/dev/null | head -n 10
-) | dzen2 -p -x $x_pos -y 20 -w 290 -l 10 \
+khal list 2>/dev/null | head -n $cnt
+) | dzen2 -p -x $x_pos -y 20 -w 290 -l $cnt \
         -bg "#1c1c1c" \
         -e 'onstart=uncollapse;button1=exit;button3=exit'
--- .conky/buttons/tasks.sh
+++ .conky/buttons/tasks.sh
@@ -3,9 +3,10 @@ x_pos=$(xrandr | grep -o 'current [0-9]*\ x' | sed 's/[a-z ]//g')
 x_pos=$(( $x_pos - 500 ))
 cnt=$(task +PENDING count)
 cnt=$(( $cnt + 3 ))
+cnt=$(( $cnt<15?$cnt:15 ))
 (
 echo "Open Tasks"
-task next 2>/dev/null
+task next 2>/dev/null | head -n $cnt
 ) | dzen2 -p -x $x_pos -y 20 -w 490 -l $cnt \
         -bg "#1c1c1c" \
         -e 'onstart=uncollapse;button1=exit;button3=exit'
