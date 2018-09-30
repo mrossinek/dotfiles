2e3aad377fd1964d6d0aa2974f3878ec8309025f
mrossinek <rmax@ethz.ch>
2018-09-30 11:31:10 +0200

    [conky] add battery indicator

--- .conky/buttons/bat.sh
+++ .conky/buttons/bat.sh
@@ -3,8 +3,8 @@ eval $(xdotool getmouselocation --shell)
 width=600
 x_pos=$(( $X - width + 50 ))
 (
-echo "Sensors"
-sensors
-) | dzen2 -p -x $x_pos -y 20 -w $width -l 24 \
+echo "Batteries"
+acpi
+) | dzen2 -p -x $x_pos -y 20 -w $width -l 2 \
         -bg "#1c1c1c" \
         -e 'onstart=uncollapse;button3=exit'
