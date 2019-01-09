cf7855c8e3d5a73e2cb71f99b3449e55956c5556
mrossinek <rmax@ethz.ch>
2019-01-09 17:59:19 +0100

    [conky] add notification to mail counter script

--- .conky/scripts/mail-counter.sh
+++ .conky/scripts/mail-counter.sh
@@ -2,6 +2,8 @@
 
 # run this script after fetching new mail to update unread mail count
 
+typeset -i prev=$(cat ~/.mailcount)
+
 find ~/.mail -type d -name "new" -exec sh -c '
         for dir do
                 if echo $dir | grep -viq "trash\|del\|draft"; then
@@ -10,3 +12,10 @@ find ~/.mail -type d -name "new" -exec sh -c '
         done
         echo $count
 ' sh {} + > ~/.mailcount
+
+typeset -i next=$(cat ~/.mailcount)
+
+if [ $next -gt $prev ]; then
+        notify-send -u low "Mail" "$count new mails"
+fi
+
