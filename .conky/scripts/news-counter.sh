109af729135244e20ca1039f855bfc4d15b1e1f9
mrossinek <rmax@ethz.ch>
2019-01-09 18:00:30 +0100

    [conky] add notification to news counter script

--- .conky/scripts/news-counter.sh
+++ .conky/scripts/news-counter.sh
@@ -2,4 +2,13 @@
 
 # run this script after fetching news to update unread news count
 
+typeset -i prev=$(cat ~/.newscount)
+
 newsboat -x print-unread | sed 's/[^0-9]//g' > ~/.newscount
+
+typeset -i next=$(cat ~/.newscount)
+
+if [ $next -gt $prev ]; then
+        notify-send -u low "News" "$count new news"
+fi
+
