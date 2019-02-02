6148bc8442dca35769b942aa297a7eeb9793d69a
mrossinek <rmax@ethz.ch>
2019-02-02 10:27:44 +0100

    [conky] minor changes

--- .conky/buttons/music.sh
+++ .conky/buttons/music.sh
@@ -2,11 +2,12 @@
 eval $(xdotool getmouselocation --shell)
 width=600
 x_pos=$(( $X - width + 50 ))
+cnt=$(mpc playlist | wc -l)
 (
 #echo "Music Controls"
 #conky -c ~/.conky/buttons/music.conkyrc
 echo "MPC Playlist"
-mpc playlist
-) | dzen2 -p -x $x_pos -y 20 -w $width -l 10 \
+mpc playlist | nl
+) | dzen2 -p -x $x_pos -y 20 -w $width -l $cnt \
         -bg "#1c1c1c" \
         -e 'onstart=uncollapse;button3=exit'
