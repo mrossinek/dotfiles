dca2ed66f2ad8ca8933a33a3e36a052650b75d06
mrossinek <rmax@ethz.ch>
2019-03-04 09:46:11 +0100

    [qutebrowser] add tabs toggle shortcuts

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -32,6 +32,8 @@ config.bind('<Alt+p>', 'spawn --userscript qute-pass')
 config.bind('A', 'spawn buku -a {url}')
 config.bind('M', 'hint links spawn mpv {hint-url}')
 config.bind('m', 'spawn mpv {url}')
+config.bind('xt', 'set tabs.show never')
+config.bind('xT', 'set tabs.show always')
 
 # search engines
 c.url.searchengines = {'DEFAULT': 'https://duckduckgo.com/?q={}', 'ap': 'https://www.archlinux.org/packages/?q={}', 'archwiki': 'https://wiki.archlinux.org/?search={}', 'arxiv': 'https://arxiv.org/search/?query={}', 'aur': 'https://aur.archlinux.org/packages/?K={}', 'aw': 'https://wiki.archlinux.org/?search={}', 'cpp': 'https://en.cppreference.com/mwiki/index.php?title=Special%3ASearch&search={}&button=', 'd': 'https://www.dict.cc/?s={}', 'dict': 'https://www.dict.cc/?s={}', 'g': 'https://www.github.com/search?&q={}', 'github': 'https://www.github.com/search?&q={}', 'package': 'https://www.archlinux.org/packages/?q={}', 'w': 'https://en.wikipedia.org/w/index.php?search={}&title=Special:Search', 'wikipedia': 'https://en.wikipedia.org/w/index.php?search={}&title=Special:Search', 'x': 'https://hayoo.fh-wedel.de/?query={}', 'xmonad': 'https://hayoo.fh-wedel.de/?query={}', 'y': 'https://www.youtube.com/results?search_query={}', 'youtube': 'https://www.youtube.com/results?search_query={}'}
