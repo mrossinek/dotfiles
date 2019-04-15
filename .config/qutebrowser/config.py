7a0081c0e415acd08aa8598de93a672ae9d62d37
mrossinek <rmax@ethz.ch>
2019-04-15 09:20:55 +0200

    minor changes

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,3 +1,7 @@
+# pylint: disable=C0111
+c = c  # noqa: F821 pylint: disable=E0602,C0103
+config = config  # noqa: F821 pylint: disable=E0602,C0103
+
 # load autoconfig
 config.load_autoconfig()
 
@@ -83,4 +87,3 @@ c.fonts.messages.warning = '14pt monospace'
 c.fonts.prompts = '14pt monospace'
 c.fonts.statusbar = '14pt monospace'
 c.fonts.tabs = '14pt monospace'
-
