d10352173635807a6862e14089a3da89e7fdc9dd
mrossinek <rmax@ethz.ch>
2019-08-12 12:12:32 +0200

    [dzen/conky] reduce clickable areas

--- .config/conky/buttons/productivity_toggle.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ ! -f ~/.productivity_timer ]
-then
-        touch ~/.productivity_timer
-else
-        rm ~/.productivity_timer
-fi
--- .config/conky/buttons/stocks.sh
+++ .config/conky/buttons/stocks.sh
@@ -6,6 +6,6 @@ cnt=$(cat ~/.stocks/prices.txt | wc -l)
 (
 echo "Current Stock prices"
 cat ~/.stocks/prices.txt
-) | dzen2 -p -x $x_pos -y 20 -w $width -l $cnt -sa l -ta c \
+) | dzen2 -p -x $x_pos -y 20 -w $width -l $cnt -sa c -ta c \
         -bg "#1c1c1c" \
         -e 'onstart=uncollapse;button1=exit;button3=exit'
