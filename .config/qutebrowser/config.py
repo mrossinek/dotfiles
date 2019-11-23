903d96186e100d3ed8dd8741db59a7598cb871bd
mrossinek <rmax@ethz.ch>
2019-11-23 14:16:32 +0100

    [qutebrowser] make mode persistent per tab

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -28,6 +28,7 @@ c.tabs.position = 'left'
 c.tabs.width = '15%'
 c.tabs.padding = {'bottom': 0, 'left': 2, 'right': 0, 'top': 0}
 c.tabs.select_on_remove = 'last-used'
+c.tabs.mode_on_change = 'restore'
 c.colors.tabs.bar.bg = '#303030'
 c.colors.tabs.even.fg = 'black'
 
