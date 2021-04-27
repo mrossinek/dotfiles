c623311c64ceea6b2d6291507b6825a820009fc2
Max Rossmannek <max.rossmannek@uzh.ch>
2021-04-27 17:51:54 +0200

    [zsh] basic jupyter settings

---
+++ zsh/.jupyter/jupyter_qtconsole_config.py
@@ -0,0 +1,8 @@
+c.JupyterConsoleApp.confirm_exit = False
+c.JupyterQtConsoleApp.confirm_exit = False
+c.JupyterQtConsoleApp.hide_menubar = True
+c.ConsoleWidget.include_other_output = True
+c.HistoryConsoleWidget.include_other_output = True
+c.FrontendWidget.include_other_output = True
+c.JupyterWidget.include_other_output = True
+c.JupyterWidget.syntax_style = 'native'
