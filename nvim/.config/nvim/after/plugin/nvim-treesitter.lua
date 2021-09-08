2f9112c58f952172f5ae11869b5e8edec5e5c546
Max Rossmannek <max.rossmannek@uzh.ch>
2021-09-08 21:18:39 +0200

    [nvim] Update neorg submodules

--- nvim/.config/nvim/after/plugin/nvim-treesitter.lua
+++ nvim/.config/nvim/after/plugin/nvim-treesitter.lua
@@ -2,7 +2,7 @@ local parser_configs = require('nvim-treesitter.parsers').get_parser_configs()
 
 parser_configs.norg = {
     install_info = {
-        url = "https://github.com/vhyrro/tree-sitter-norg",
+        url = "https://github.com/nvim-neorg/tree-sitter-norg",
         files = { "src/parser.c", "src/scanner.cc" },
         branch = "main"
     },
