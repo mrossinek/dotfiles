2535a7f49b931b98343c905acf11732ff5cb2b4a
Max Rossmannek <max.rossmannek@uzh.ch>
2021-11-21 22:12:47 +0100

    [nvim] ensure additional neorg TS parsers are installed

--- nvim/.config/nvim/after/plugin/nvim-treesitter.lua
+++ nvim/.config/nvim/after/plugin/nvim-treesitter.lua
@@ -4,7 +4,23 @@ parser_configs.norg = {
     install_info = {
         url = "https://github.com/nvim-neorg/tree-sitter-norg",
         files = { "src/parser.c", "src/scanner.cc" },
-        branch = "main"
+        branch = "main",
+    },
+}
+
+parser_configs.norg_meta = {
+    install_info = {
+        url = "https://github.com/nvim-neorg/tree-sitter-norg-meta",
+        files = { "src/parser.c" },
+        branch = "main",
+    },
+}
+
+parser_configs.norg_table = {
+    install_info = {
+        url = "https://github.com/nvim-neorg/tree-sitter-norg-table",
+        files = { "src/parser.c" },
+        branch = "main",
     },
 }
 
@@ -20,6 +36,8 @@ require'nvim-treesitter.configs'.setup {
         "json",
         "lua",
         "norg",
+        "norg_meta",
+        "norg_table",
         "python",
         "yaml",
     },
