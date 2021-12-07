b65e2b67cfcb69ee231c6a1af8b744cf9c057157
Max Rossmannek <max.rossmannek@uzh.ch>
2021-12-07 21:23:35 +0100

    [nvim] replace tcomment_vim with comment.nvim

---
+++ nvim/.config/nvim/after/plugin/comment.nvim.lua
@@ -0,0 +1,17 @@
+require('Comment').setup({
+    toggler = {
+        line = 'gcc',
+        block = 'gbc',
+    },
+
+    opleader = {
+        line = 'gc',
+        block = 'gb',
+    },
+
+    mappings = {
+        basic = true,
+        extra = true,
+        extended = true,
+    },
+})
