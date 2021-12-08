69112ac81efcadf9daa0e085261c04de12f041b5
Max Rossmannek <max.rossmannek@uzh.ch>
2021-12-08 14:38:36 +0100

    [nvim] handle neorg dev environment of TS parsers

--- nvim/.config/nvim/after/plugin/nvim-treesitter.lua
+++ nvim/.config/nvim/after/plugin/nvim-treesitter.lua
@@ -1,28 +1,51 @@
 local parser_configs = require('nvim-treesitter.parsers').get_parser_configs()
 
-parser_configs.norg = {
-    install_info = {
-        url = "https://github.com/nvim-neorg/tree-sitter-norg",
-        files = { "src/parser.c", "src/scanner.cc" },
-        branch = "main",
-    },
-}
+if (os.getenv("NEORG_DEV") ~= nil) then
+    parser_configs.norg = {
+        install_info = {
+            url = "~/Git/tree-sitter-norg",
+            files = { "src/parser.c", "src/scanner.cc" },
+        },
+    }
 
-parser_configs.norg_meta = {
-    install_info = {
-        url = "https://github.com/nvim-neorg/tree-sitter-norg-meta",
-        files = { "src/parser.c" },
-        branch = "main",
-    },
-}
+    parser_configs.norg_meta = {
+        install_info = {
+            url = "~/Git/tree-sitter-norg-meta",
+            files = { "src/parser.c" },
+        },
+    }
 
-parser_configs.norg_table = {
-    install_info = {
-        url = "https://github.com/nvim-neorg/tree-sitter-norg-table",
-        files = { "src/parser.c" },
-        branch = "main",
-    },
-}
+    parser_configs.norg_table = {
+        install_info = {
+            url = "~/Git/tree-sitter-norg-table",
+            files = { "src/parser.c" },
+        },
+    }
+else
+    parser_configs.norg = {
+        install_info = {
+            url = "https://github.com/nvim-neorg/tree-sitter-norg",
+            files = { "src/parser.c", "src/scanner.cc" },
+            branch = "main",
+        },
+    }
+
+    parser_configs.norg_meta = {
+        install_info = {
+            url = "https://github.com/nvim-neorg/tree-sitter-norg-meta",
+            files = { "src/parser.c" },
+            branch = "main",
+        },
+    }
+
+    parser_configs.norg_table = {
+        install_info = {
+            url = "https://github.com/nvim-neorg/tree-sitter-norg-table",
+            files = { "src/parser.c" },
+            branch = "main",
+        },
+    }
+end
 
 require'nvim-treesitter.configs'.setup {
     ensure_installed = {
