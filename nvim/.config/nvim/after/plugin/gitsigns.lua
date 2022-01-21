b29b2fe151a90eb6c560de80b97469ca56d659a9
Max Rossmannek <max.rossmannek@uzh.ch>
2022-01-21 19:03:18 +0100

    [nvim] add abbreviated SHA to gitsign blame

--- nvim/.config/nvim/after/plugin/gitsigns.lua
+++ nvim/.config/nvim/after/plugin/gitsigns.lua
@@ -1,6 +1,32 @@
 require('gitsigns').setup {
     numhl = true,
     current_line_blame = true,
+    current_line_blame_formatter_opts = {
+        abbrev_sha = true,
+    },
+    current_line_blame_formatter = function(name, blame_info, opts)
+        if blame_info.author == name then
+            blame_info.author = 'You'
+        end
+
+        local text
+        if blame_info.author == 'Not Committed Yet' then
+            text = blame_info.author
+        else
+            local date_time
+
+            date_time = os.date('%Y-%m-%d', tonumber(blame_info['author_time']))
+
+            local abbrev_sha
+            if opts.abbrev_sha then
+                abbrev_sha = blame_info.abbrev_sha
+            end
+
+            text = string.format('%s, %s - %s @ %s', blame_info.author, date_time, blame_info.summary, abbrev_sha)
+        end
+
+        return {{' '..text, 'GitSignsCurrentLineBlame'}}
+    end,
     sign_priority = 5,
     word_diff = false,
 }
