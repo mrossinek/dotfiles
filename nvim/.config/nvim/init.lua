9c53671f9147d9ea5ba3cc584d27e1ffa72aba26
Max Rossmannek <max@rossmannek.de>
2024-06-08 12:52:26 +0200

    [nvim] disable unused providers

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -8,7 +8,10 @@ vim.keymap.set("n", "<space>", "<Nop>")
 vim.g["mapleader"] = " "
 vim.g["maplocalleader"] = "\\"
 
+-- providers
 vim.g["python3_host_prog"] = "~/Installations/pynvim/venv/bin/python"
+vim.g["loaded_perl_provider"] = 0
+vim.g["loaded_ruby_provider"] = 0
 
 if vim.api.nvim_eval("exists('$EXTRA_VIM')") == 1 then
     for _, path in pairs(vim.split(os.getenv("EXTRA_VIM"), ":")) do
