e83f7d5a87ef94e2c28dfbc00a9db173923f2543
Max Rossmannek <max@rossmannek.de>
2025-07-17 07:02:28 +0200

    [nvim] remove broken TextYankPost config

--- nvim/.config/nvim/plugin/autocommands.lua
+++ nvim/.config/nvim/plugin/autocommands.lua
@@ -1,12 +1,14 @@
 local flash_yanked = vim.api.nvim_create_augroup("FlashYanked", { clear = true })
 
-vim.api.nvim_create_autocmd("TextYankPost", {
-    group = flash_yanked,
-    callback = function() require'vim.highlight'.on_yank({
-        higroup="ExtraWhitespace",
-        timeout=200,
-    }) end,
-})
+-- FIXME: make compatible with nvim 0.11
+--
+-- vim.api.nvim_create_autocmd("TextYankPost", {
+--     group = flash_yanked,
+--     callback = function() require'vim.highlight'.on_yank({
+--         higroup="ExtraWhitespace",
+--         timeout=200,
+--     }) end,
+-- })
 
 local restore_cursor = vim.api.nvim_create_augroup("RestoreCursor", { clear = true })
 
